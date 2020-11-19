963e5ca5d86617da5288948076ec40f2bb022550
Tomas Gomes <tvb.gomes@gmail.com>
2020-11-19 13:36:52 -0300

    created routes for cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :cocktails do
+    resources :ingredients, only: %i[index edit]
+    resources :doses, only: %i[index edit]
+  end
 end
