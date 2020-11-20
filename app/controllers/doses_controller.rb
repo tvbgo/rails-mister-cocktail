608d1ca23db2d197675e2aaf5c2e03f0fe3dcb58
Tomas Gomes <tvb.gomes@gmail.com>
2020-11-20 08:53:42 -0300

    created Doses controller and controller actions

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+  before_action :find_dose, only: [:show, :destroy]
+
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+
+  def find_dose
+    @dose = Dose.find(params[:id])
+  end
+end
