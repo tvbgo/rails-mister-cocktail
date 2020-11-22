bfe0572b443365a1e8255c52207f6c95febd1bcd
Tomas Gomes <tvb.gomes@gmail.com>
2020-11-22 16:09:36 -0300

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@
 require 'faker'
 require 'open-uri'
 
-puts 'Droping database...'
 Cocktail.destroy_all
 Ingredient.destroy_all
 Dose.destroy_all
@@ -17,8 +16,33 @@ puts 'Creating new cocktail ingredients...'
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 url_file = open(url).read
 @ingredients = JSON.parse(url_file)
-@ingredients["drinks"].each do |ingredient|
-  new_ingredient = Ingredient.create(name: ingredient["strIngredient1"])
-  puts "Created #{new_ingredient.name}"
+@ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
 end
 
+15.times do
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  url_file = open(url).read
+  @drinks = JSON.parse(url_file)
+  @name = @drinks['drinks'][0]['strDrink']
+  @photo_url = @drinks['drinks'][0]['strDrinkThumb']
+  file = URI.open(@photo_url)
+  cocktail = Cocktail.create(
+    {
+      name: @name
+    }
+  )
+  cocktail.photo.attach(io: file, filename: 'new.jpg', content_type: 'image/jpg')
+  puts "created #{cocktail.name}"
+end
+
+60.times do
+  dose = Dose.create(
+    {
+      description: Faker::Food.measurement,
+      cocktail_id: rand(1..15),
+      ingredient_id: rand(1..100)
+    }
+  )
+  puts "Added #{dose.description} of #{dose.ingredient[:name]} to #{dose.cocktail[:name]}"
+end
